d7e5af290aaa54d2b80e54e1288cc111f909ea08
chensj <mrcsj@tom.com>
2017-09-11 11:08:54 +0800

    增加RestTemplate配置

--- web/src/main/java/com/test/web/WebApplication.java
+++ web/src/main/java/com/test/web/WebApplication.java
@@ -3,10 +3,13 @@ package com.test.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -19,4 +22,9 @@ public class WebApplication {
         SpringApplication.run(WebApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
